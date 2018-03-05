80819d7fe113630069a7ea2d560f52e0f1dd19ab
Paul Fitzgerald <paulfitz99@gmail.com>
2018-03-05 15:58:06 +0000

    add repository card

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,22 +1,55 @@
 import React from 'react';
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
+import styled from 'styled-components'
 
 const Repo = ({ data: { viewer }}) => {
 
   const repos = viewer ? viewer.repositories.edges.map(repo => {
     return (
-      <p>{ repo.node.name }</p>
+      <RepoCard key={ repo.node.name }>
+        <RepoLink>{ repo.node.name }</RepoLink>
+        <RepoDescription>{ repo.node.description }</RepoDescription>
+        <RepoDetails>{ repo.node.languages.edges[0].node.name } { repo.node.stargazers.totalCount } { repo.node.forkCount }</RepoDetails>
+      </RepoCard>
     )
   }) : []
-  return (
-    <section>
 
+  return (
+    <RepoContainer>
       { repos }
-  
-    </section>
+    </RepoContainer>
   )
 }
+
+const RepoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
+
+const RepoCard = styled.div`
+  border: 1px #d1d5da solid;
+  padding: 16px;
+  width: 450px;
+  margin-bottom: 16px;
+`
+
+const RepoDescription = styled.p`
+  font-size: 12px;
+  color: #586069;
+`
+
+const RepoLink = styled.a`
+  font-weight: 600;
+  font-size: 14px;
+  color: #0366d6;
+`
+
+const RepoDetails = styled.span`
+  color: #586069;
+  font-size: 12px;
+`
 
 export default graphql(gql`
 query {
@@ -25,7 +58,15 @@ query {
       edges {
         node {
           name
-          description   
+          description 
+          languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+            edges {
+              node {
+                name
+              }
+            }
+          }  
+          forkCount
           stargazers {
             totalCount
           }
